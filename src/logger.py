e4442d66a4a903e9dd4f0ebf943de1ae7df11421
Huy <huy@hhoang.dev>
2020-12-16 13:53:54 +0100

    logs folder not in src but above

--- src/logger.py
+++ src/logger.py
@@ -2,33 +2,35 @@ import logging
 import datetime
 import os
 
-if not os.path.isdir('./logs'):
-	os.mkdir('./logs')
+folder_path = '../logss'
+
+if not os.path.isdir(folder_path):
+	os.mkdir(folder_path)
 
 log = logging.getLogger('root')
 log.setLevel(logging.DEBUG)
 
-file_formatter = logging.Formatter(
+formatter = logging.Formatter(
 	'%(levelname)7s|%(asctime)s|%(funcName)20s()|line %(lineno)3s|%(message)3s',
 	datefmt='%Y-%m-%d %H:%M:%S'
 )
 
 def file_handler(folder, log_level):
-	if not os.path.isdir(f'./logs/{folder}'):
-		os.mkdir(f'./logs/{folder}')
+	if not os.path.isdir(f'{folder_path}/{folder}'):
+		os.mkdir(f'{folder_path}/{folder}')
 
 	handler = logging.FileHandler(
-							f'logs/{folder}/' 
+							f'{folder_path}/{folder}/' 
 							+ datetime.datetime.now().strftime("%Y-%m-%d") 
 							+ '.log')
 	handler.setLevel(log_level)
-	handler.setFormatter(file_formatter)
+	handler.setFormatter(formatter)
 	log.addHandler(handler)
 
-file_handler('verbose', logging.DEBUG)
+file_handler('debug', logging.DEBUG)
 file_handler('info', logging.INFO)
 
 consoleHandler = logging.StreamHandler()
-consoleHandler.setFormatter(file_formatter)
-consoleHandler.setLevel(logging.INFO)
+consoleHandler.setFormatter(formatter)
+consoleHandler.setLevel(logging.DEBUG)
 log.addHandler(consoleHandler)
