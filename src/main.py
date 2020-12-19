6fcc2e6c65787084fcc0f14075376bda3c00d57e
Huy <huy@hhoang.dev>
2020-12-19 17:31:28 +0100

    update all playlist and restore function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from enum import Enum
 
 import spotipy
@@ -17,8 +18,8 @@ class PlaylistType(Enum):
 
 
 
-def find_playlist_in_live_playlists(uri, playlists):
-	for playlist in playlists['items']:
+def find_playlist_in_live_playlists(uri):
+	for playlist in all_playlists['items']:
 		if playlist['uri'] == uri:
 			return playlist
 	raise Exception('Playlist not found.')
@@ -28,80 +29,137 @@ def has_playlist_changed(playlist_data, live_playlist):
 		return True
 	return False
 
-def get_tracks_in_all_playlist(playlist):
 
-	num_tracks_in_playlist = playlist['tracks']['total']
+def get_tracks_in_playlist(playlist_uri, limit=None, offset=None):
+	if offset is not None:
+		playlist = find_playlist_in_live_playlists(playlist_uri)
+		num_tracks_in_playlist = playlist['tracks']['total']
+		offset = num_tracks_in_playlist - 50
+		if offset < 0:
+			offset = 0
 
-	limit = 50
-	offset = num_tracks_in_playlist - 50
-	tracks = sp.playlist_items(playlist['id'],
+	tracks = sp.playlist_items(playlist_uri,
 														 fields='items',
-														 offset=offset,
-														 limit=limit)['items']
-
+														 limit=limit,
+														 offset=offset
+														 )['items']
 	tracks.reverse()
-	
 	return tracks
 
-def update_snippet_playlist(playlist_data, live_playlist):
+def update_snippet_playlist(playlist_data):
 	get_track_ids = lambda tracks: [track['track']['id'] for track in tracks]
 
-	most_recent_tracks = get_tracks_in_all_playlist(live_playlist)
+	most_recent_tracks = get_tracks_in_playlist(playlist_data['all']['uri'], limit=50, offset=50)
 	track_ids = get_track_ids(most_recent_tracks)
-	sp.playlist_replace_items(playlist_data['uri'], track_ids)
+	sp.playlist_replace_items(playlist_data['snippet']['uri'], track_ids)
 
 
-def get_tracks_in_snippet_playlist(playlist):
-	tracks = sp.playlist_items(playlist['id'], fields='items')['items']
-	tracks.reverse()
-	return tracks
 
-def update_all_playlist(playlist_data, live_playlist):
+
+def update_all_playlist(playlist_data):
 	get_track_ids = lambda tracks: [track['track']['id'] for track in tracks]
 
-	tracks = get_tracks_in_snippet_playlist(live_playlist)
-	track_ids = get_track_ids(tracks)
-
-	all_tracks = sp.playlist_items(playlist_data['uri'], fields='items')['items']
-
-	# sp.playlist_replace_items(playlist_data['uri'], track_ids)
-
-def update_snapshot_of_playlist(playlist_to_update):
-	live_updated_playlist = find_playlist_in_live_playlists(playlist_to_update['uri'], all_playlists)
-	playlist_to_update['snapshot_id'] = live_updated_playlist['snapshot_id']
+	snippet_tracks = get_tracks_in_playlist(playlist_data['snippet']['uri'])
+	snippet_track_ids = get_track_ids(snippet_tracks)
 	
+	all_tracks = get_tracks_in_playlist(playlist_data['all']['uri'])
+	all_track_ids = get_track_ids(all_tracks)
 
-def check_for_changes(playlist_to_check_id: PlaylistType, all_playlists):
+	def not_in(a_list):
+		def fn(val):
+			if val in a_list:
+				return False
+			return True
+		return fn
+
+	new_tracks = list(filter(not_in(all_track_ids), snippet_track_ids))
+	removed_tracks = list(filter(not_in(snippet_track_ids), all_track_ids))
+	# new_tracks.reverse()
+	# removed_tracks.reverse()
+
+	sp.playlist_remove_all_occurrences_of_items(playlist_data['all']['uri'], removed_tracks)
+	sp.playlist_add_items(playlist_data['all']['uri'], new_tracks)
+
+
+	for track_id in new_tracks:
+		for t in snippet_tracks:
+			if track_id == t['track']['id']:
+				log.debug(f"new track: {t['track']['name']}")
+				break
+
+	for track_id in removed_tracks:
+		for t in all_tracks:
+			if track_id == t['track']['id']:
+				log.debug(f"removed track: {t['track']['name']}")
+				break
+
+	log.debug(f'{new_tracks=}')
+	log.debug(f'{removed_tracks=}')
+
+
+
+
+def update_snapshot_of_playlist(playlist, playlists_data, fetch_new=True):
+	global all_playlists
+	if fetch_new:
+		all_playlists = sp.current_user_playlists()
+
+	live_playlist = find_playlist_in_live_playlists(playlist['uri'])
+	playlist['snapshot_id'] = live_playlist['snapshot_id']
+	helpers.set_playlist_data(playlists_data)
+
+
+def check_for_changes(playlist_to_check_id: PlaylistType):
 	# TODO: refactor, this function is too big
 	playlists_data = helpers.get_playlist_data()
 	for playlist_data in playlists_data:
-		live_playlist = find_playlist_in_live_playlists(playlist_data[playlist_to_check_id.value]['uri'], all_playlists)
+		live_playlist = find_playlist_in_live_playlists(playlist_data[playlist_to_check_id.value]['uri'])
 
 		playlist_to_check = playlist_data[playlist_to_check_id.value]
 		if has_playlist_changed(playlist_to_check, live_playlist):
 			log.info(f"{playlist_data['name']} has changed. Adding new songs") # TODO: display newly added songs in log
 
-			playlist_to_check['snapshot_id'] = live_playlist['snapshot_id']
-			helpers.set_playlist_data(playlists_data)
+			update_snapshot_of_playlist(playlist_to_check, playlists_data, fetch_new=False)
 		
 			if playlist_to_check_id == PlaylistType.ALL:
 				playlist_to_update = playlist_data[PlaylistType.SNIPPET.value]
-				update_snippet_playlist(playlist_to_update, live_playlist)
+				update_snippet_playlist(playlist_data)
 			else:
 				playlist_to_update = playlist_data[PlaylistType.ALL.value]
-				update_all_playlist(playlist_to_update, live_playlist)
+				update_all_playlist(playlist_data)
 
-			update_snapshot_of_playlist(playlist_to_update)
-			helpers.set_playlist_data(playlists_data)
+			update_snapshot_of_playlist(playlist_to_update, playlists_data)
+			
 		else:
 			log.debug(f"{playlist_data['name']} hasn't changed.")	
 
 
-def main():
-	log.info('')
-	all_playlists = sp.current_user_playlists()
-	# check_for_changes(PlaylistType.ALL, all_playlists)
-	check_for_changes(PlaylistType.SNIPPET, all_playlists)
+
+def restore():
+
+	playlist_to_restore = 'spotify:playlist:48tPqcYezzxCJfEZdQiuEj'
+	backup = 'spotify:playlist:7uuUFU3ZPeldfEjKJ94jwX'
+
+
+	amount_tracks = sp.playlist_items(backup)['total']
+
+	position = 0
+	for offset in range(0, amount_tracks, 100):
+		# print(offset)
+		offset = amount_tracks - (offset + 100)
+		if offset < 0:
+			offset = 0
+		print(offset)
+		tracks = sp.playlist_items(backup, offset=offset, fields='items')['items']
+		tracks.reverse()
+
+		for track in tracks:
+			log.debug(f"{track['track']['name']}")
+			sp.playlist_add_items(playlist_to_restore, [track['track']['id']], position=position)
+			position += 1
+			time.sleep(1)
+
+	# helpers.write_dict_to_file('data', amount_tracks)
 
 
 if __name__ == '__main__':
@@ -112,4 +170,12 @@ if __name__ == '__main__':
                                                  client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
 																							   redirect_uri=redirect_uri, scope=scope))
 
-	main()
+	log.info('')
+	# all_playlists = sp.current_user_playlists()
+
+
+	# restore()
+
+	check_for_changes(PlaylistType.ALL)
+	check_for_changes(PlaylistType.SNIPPET)
+
