c079f771e5af4c97cae598fa1f0f057d1c6cd6f1
Huy <huy@hhoang.dev>
2021-09-09 22:46:05 +0200

    clean up

--- src/helpers.py
+++ src/helpers.py
@@ -1,19 +1,35 @@
 import json
 
-def get_playlist_data():
-	with open('../playlists.json') as f:
-		return json.load(f)
-def set_playlist_data(playlists_data):
-	with open('../playlists.json', 'w') as f:
-		f.write(json.dumps(playlists_data, indent=2))
-
-def write_dict_to_file(name, data):
-	with open(f'../{name}.json', 'w') as f:
+def write_dict_to_file(name: str, data):
+	with open(f'./data/{name}.json', 'w') as f:
 		f.write(json.dumps(data, indent=2))
 
+def read_dict_from_file(name: str):
+	with open(f'./data/{name}.json') as f:
+		json.load(f)
 
 def get_track_ids(tracks):
 	return [track['track']['id'] for track in tracks]
 
 def get_track_names(tracks: list):
 	return [track['track']['name'] for track in tracks]
+
+
+
+def create_data_class(data: dict):
+	for k,v in data.items():
+		print(f'{k}: {type(v).__name__}')
+		type_ = type(v).__name__
+		if type_ == 'list':
+			print('=============')
+			create_data_class(v[0])
+		elif type_ == 'dict':
+			print('=============')
+			create_data_class(v)
+
+if __name__ == '__main__':
+	with open('../data/playlists.json') as f:
+		playlists = f.read()
+	playlists = json.loads(playlists)
+	create_data_class(playlists)
+	
