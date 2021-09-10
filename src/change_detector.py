01133f11c21f27ef9c0d9e5cb4ddb1a0cd3d7f20
Huy <huy@hhoang.dev>
2021-09-10 22:00:02 +0200

    init snippet logic

--- src/change_detector.py
+++ src/change_detector.py
@@ -1,21 +1,28 @@
 import json
+from enum import Enum
 
 from .playlist_handler import Playlists
-from .data_types import TrackedPlaylistsType, TrackedPlaylistsTypeHelper
+from .data_types import TrackedPlaylistClusterType, TrackedPlaylistType
 
-with open('./playlists.json') as f:
-	tracked_playlists: list[TrackedPlaylistsType] = json.load(f)
-	
-def check_changes(live_playlists: Playlists):
-	for tracked in tracked_playlists:
-		if has_changed(tracked['snippet'], live_playlists):
-			pass
+class PlaylistType(Enum):
+	All = 'all'
+	Snippet = 'snippet'
+		
+class ChangeDetector:
+	def __init__(self, live_playlists: Playlists):
+		self.live_playlists = live_playlists
+		with open('./playlists.json') as f:
+			self.tracked_clusters: list[TrackedPlaylistClusterType] = json.load(f)
+			
+	def check_for_changes(self):
+		for cluster in self.tracked_clusters:
+			if self._has_changed(cluster['snippet']):
+				pass
 
-		if has_changed(tracked['all'], live_playlists):
-			pass
+			if self._has_changed(cluster['all']):
+				pass
 
 
-def has_changed(tracked: TrackedPlaylistsTypeHelper,
-								live_playlists: Playlists) -> bool:
-	live = live_playlists.get_by_uri(tracked['uri'])
-	return (tracked['snapshot_id'] != live.snapshot_id)
+	def _has_changed(self, tracked: TrackedPlaylistType) -> bool:
+		live = self.live_playlists.get_by_uri(tracked['uri'])
+		return tracked['snapshot_id'] != live.snapshot_id
