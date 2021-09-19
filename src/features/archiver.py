2aeadbcedb19bae21b22d7455121025a69bea5d5
Huy <huy@hhoang.dev>
2021-09-19 14:43:07 +0200

    implemented general logic for archiving

--- src/features/archiver.py
+++ src/features/archiver.py
@@ -1,11 +1,11 @@
-from src.playlists import LivePlaylists
-from src.data_types import TrackedPlaylistType
-from src.api_handler import sp
+from src.playlists.live_playlists import LivePlaylists
+
+from src.playlists.tracked_playlists import TrackedPlaylists, TrackedPlaylist
 from src.tracks import Tracks
 
 class Archiver:
 	"""
-	Take the oldest songs from a playlist that exceed the amount of tracks in
+	Take the oldest songs from playlists that exceed the amount of tracks in
 	a playlist, remove them, and put them into the corresponding archive playlist.
 
 	This class doesnt know whether a playlist has changed or not,
@@ -13,20 +13,32 @@ class Archiver:
 	"""
 
 	def __init__(self, live_playlists: LivePlaylists,
-										 playlist: TrackedPlaylistType):
+										 tracked_playlists: TrackedPlaylists):
 		self.live_playlists = live_playlists
-		self.playlist = playlist
+		self.tracked_playlists = tracked_playlists
 
 		self.tracks_amount = 50
 
+	def check_for_changes(self):
+		for tracked in self.tracked_playlists.tracked:
+			live_playlist = self.live_playlists.get_by_uri(tracked.current_uri)
+			if tracked.has_changed(live_playlist):
+				self.on_update()
 
-	def on_update(self):
-		tracks = self.get_current_tracks()
+
+	def on_update(self, tracked_playlist: TrackedPlaylist):
+		current_uri = tracked_playlist.current_uri
+		archive_uri = tracked_playlist.archive_uri
+		current_playlist = self.live_playlists.get_by_uri(current_uri)
+		archive_playlist = self.live_playlists.get_by_uri(archive_uri)
+
+		tracks = current_playlist.get_latest_tracks(None)
+
 		if len(tracks) > self.tracks_amount:
 			# one or more tracks were added to playlist
 			archive_tracks = tracks.tracks[self.tracks_amount-1:]
-			sp.remove_tracks(self.playlist['current'], archive_tracks)
-			sp.add_tracks_at_beginning(self.playlist['archive'], archive_tracks)
+			archive_playlist.add_tracks(archive_tracks)
+			current_playlist.remove_tracks(archive_tracks)
 			# TODO: check if order is correct
 
 		elif len(tracks) < self.tracks_amount:
@@ -35,12 +47,6 @@ class Archiver:
 		elif len(tracks) == self.tracks_amount:
 			# an equivalent amount of songs were added and removed
 			pass
-
-
-	def get_current_tracks(self):
-		live_playlist = self.live_playlists.get_by_uri(self.playlist['current'])
-		tracks = live_playlist.get_latest_tracks(None)
-		return tracks
 
 
 	def calculate_playlist_length_with_duration(self):
