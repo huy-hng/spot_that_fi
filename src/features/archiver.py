61ff6b19ebfe63df501bc57d50d84176b8ce787e
Huy <huy@hhoang.dev>
2021-09-23 18:51:21 +0200

    implement logic for limiting current playlists by either amount or duration

--- src/features/archiver.py
+++ src/features/archiver.py
@@ -1,5 +1,5 @@
 from src.playlists.live_playlists import LivePlaylists
-
+from src.tracks import Tracks
 from src.playlists.tracked_playlists import TrackedPlaylists, TrackedPlaylist
 
 class Archiver:
@@ -11,45 +11,51 @@ class Archiver:
 	it archives the songs from current playlist
 	"""
 
-	def __init__(self, live_playlists: LivePlaylists,
-										 tracked_playlists: TrackedPlaylists):
-		self.live_playlists = live_playlists
+	def __init__(self, tracked_playlists: TrackedPlaylists):
 		self.tracked_playlists = tracked_playlists
 
 		self.TRACKS_AMOUNT = 50
-		self.DURATION = 3 * 60 * 60 # 3 hours
+		self.DURATION_IN_MINUTES = 3 * 60 # 3 hours
 
 
-	def check_for_changes(self):
+	def check_for_changes(self, live_playlists: LivePlaylists):
+		self.live_playlists = live_playlists
 		for tracked in self.tracked_playlists.playlists:
 			live_playlist = self.live_playlists.get_by_uri(tracked.current_uri)
 			if tracked.has_changed(live_playlist):
-				self.update()
+				self._update()
 
 
-	def update(self, tracked_playlist: TrackedPlaylist):
+	def _update(self, tracked_playlist: TrackedPlaylist):
 		current = self.live_playlists.get_by_uri(tracked_playlist.current_uri)
 		archive = self.live_playlists.get_by_uri(tracked_playlist.archive_uri)
 
 		tracks = current.get_latest_tracks(None)
+		archive_tracks = self.tracks_to_archive_by_amount(tracks)
 
+		if len(archive_tracks) == 0:
+			return
+
+		archive.add_tracks_at_end(archive_tracks)
+		current.remove_tracks(archive_tracks)
+
+	# TEST: check if order is correct
+	# TODO: no duplicates should be added
+
+	def tracks_to_archive_by_amount(self, tracks: list):
 		if len(tracks) > self.TRACKS_AMOUNT:
-			# one or more tracks were added to playlist
-			archive_tracks = tracks.tracks[self.TRACKS_AMOUNT-1:]
+			return tracks[self.TRACKS_AMOUNT-1:]
+		return []
 
-			archive.add_tracks_at_end(archive_tracks)
-			current.remove_tracks(archive_tracks)
+	def tracks_to_archive_by_duration(self, tracks: list):
+		""" if current_duration > self.DURATION
+				remove last item in tracks and put it in archive_tracks
+				until current_duration <= self.DURATION """
+		archive_tracks = []
+		current_duration = Tracks.get_duration(tracks, 'minutes')
+		while current_duration > self.DURATION_IN_MINUTES:
+			archive_track = tracks.pop(-1)
+			archive_tracks.append(archive_track)
+			current_duration = Tracks.get_duration(tracks, 'minutes')
 
-			# TEST: check if order is correct
-			# TODO: no duplicates should be added
-
-		elif len(tracks) < self.TRACKS_AMOUNT:
-			# one or more songs were removed
-			pass
-		elif len(tracks) == self.TRACKS_AMOUNT:
-			# an equivalent amount of songs were added and removed
-			pass
-
-
-	def calculate_playlist_length_with_duration(self):
-		raise NotImplementedError()
+		return archive_tracks
