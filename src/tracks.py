6e03f6e3cca0b8287d30687e51e47fb98665f398
Huy <huy@hhoang.dev>
2021-09-27 12:51:27 -0700

    added default param for duration_scale

--- src/tracks.py
+++ src/tracks.py
@@ -10,9 +10,10 @@ class Tracks:
 		return [track['track']['id'] for track in tracks]
 
 	@staticmethod
-	def get_duration(tracks: list, duration_scale: str):
+	def get_duration(tracks: list, duration_scale: str='minutes'):
 		""" returns duration of tracks \n
-				duration scale should be 'seconds', 'minutes' or 'hours' """
+				duration scale should be 'seconds', 'minutes' or 'hours' \n
+				default is minutes """
 
 		durations_ms: list[int] = [track['track']['duration_ms'] for track in tracks]
 		duration = sum(durations_ms) / 1000
