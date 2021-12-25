c33d1b04e1878e1f38490c365ce8b9c6bebe1863
Huy <huy@hhoang.dev>
2021-12-25 23:36:47 +0100

    clean up

--- src/database/db_helpers.py
+++ src/database/db_helpers.py
@@ -5,19 +5,8 @@ from sqlalchemy.orm import Session as sess
 from sqlalchemy.orm.query import Query
 
 from src.database import Session 
-from src.database.tables import Playlist, Track, PlaylistTracksAssociation
 
 from src.logger import log
-
-
-# def create_session(function):
-# 	@wraps(function)
-# 	def wrapper(*args, **kwargs):
-# 		with Session.begin() as session:
-# 			session: sess = session
-# 			result = function(session, *args, **kwargs)
-# 		return result
-# 	return wrapper
 
 
 def does_exist(query: Query):
--- src/database/tables.py
+++ src/database/tables.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from datetime import datetime
 from sqlalchemy import Column, String, Integer, Boolean, DateTime
 from sqlalchemy.sql.schema import ForeignKey, Table
@@ -5,14 +7,13 @@ from sqlalchemy.orm import relationship
 
 from src.database import Base
 
-
 class PlaylistTracksAssociation(Base):
 	__tablename__ = 'playlist_tracks_association'
 
 	playlist_id = Column(ForeignKey('playlist.id'), primary_key=True)
 	track_id = Column(ForeignKey('track.id'), primary_key=True)
-	track = relationship('Track', back_populates='playlists')
-	playlist = relationship('Playlist', back_populates='tracks')
+	track: Track = relationship('Track', back_populates='playlists')
+	playlist: Playlist = relationship('Playlist', back_populates='tracks')
 
 	added_by = Column(String, nullable=False)
 	added_at = Column(String, nullable=False)
@@ -32,7 +33,7 @@ class Playlist(Base):
 	snapshot_id = Column(String, nullable=False)
 	owner_id = Column(String, nullable=False)
 
-	tracks: relationship = relationship('PlaylistTracksAssociation',
+	track_associations: list[PlaylistTracksAssociation] = relationship('PlaylistTracksAssociation',
 			back_populates='playlist')
 
 	def __init__(self, playlist) -> None:
@@ -53,7 +54,7 @@ class Track(Base):
 	popularity = Column(Integer, nullable=False)
 	liked = Column(Boolean, default=False)
 
-	playlists: relationship = relationship('PlaylistTracksAssociation',
+	playlists: list[PlaylistTracksAssociation] = relationship('PlaylistTracksAssociation',
 				back_populates='track')
 
 	def __init__(self, track: dict):
