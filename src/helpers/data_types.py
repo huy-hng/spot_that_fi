ad74e42aa3e9b92b4f4530cac9e6998b2abc5237
Huy <huy@hhoang.dev>
2022-04-12 15:30:23 +0200

    clear out legacy data_types

--- src/helpers/data_types.py
+++ src/helpers/data_types.py
@@ -1,62 +1,9 @@
 from dataclasses import dataclass, field
-from typing import Optional
-
-@dataclass
-class SpotifyPlaylistsOwnerType:
-	id: str
-	display_name: str
-	external_urls: str
-	href: str
-	type: str
-	uri: str
-
-@dataclass
-class SpotifyPlaylistsTracksType:
-	href: str
-	total: int
-	limit: int
-	next: None
-	offset: int
-	previous: None
-	total: int
-	type: str
-	uri: str
+from src.helpers.logger import log
 
 
-@dataclass
-class SpotifySinglePlaylistTracksType:
-	href: str
-	items: list
-	limit: int
-	total: int
-	offset: int
-	next: str
-	previous: str
 
-@dataclass
-class SpotifyPlaylistType:
-	collaborative: bool
-	description: str
-	external_urls: dict
-	followers: dict
-	href: str
-	id: str
-	images: list
-	name: str
-	owner: SpotifyPlaylistsOwnerType
-	primary_color: None 
-	public: bool
-	snapshot_id: str
-	tracks: SpotifyPlaylistsTracksType
-	type: str
-	uri: str
 
-	def __post_init__(self):
-		self.owner = SpotifyPlaylistsOwnerType(**self.owner)
-		if self.tracks.get('items') is None:
-			self.tracks = SpotifyPlaylistsTracksType(**self.tracks)
-		else:
-			self.tracks = SpotifySinglePlaylistTracksType(**self.tracks)
 
 @dataclass
 class TrackedPlaylistType:
@@ -65,8 +12,15 @@ class TrackedPlaylistType:
 	current: str
 	snapshot_id: str
 
+
+
 @dataclass
 class TracksType:
-	# TODO
 	next: str
 	items: list
+
+
+# @dataclass
+# class LikedTrack:
+# 	added_at: str # TODO: could be datetime instead
+# 	track: dict
