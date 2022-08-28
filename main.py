50da29520266fbce2171e192c49b567210dbf18b
Huy <huy@hoang.dev>
2022-08-28 23:02:36 +0200

    added some more data for typing

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from src.tests.test_snippet_feature import reset_playlists
 from src.tests import get_sample_data
 
 # reset_playlists()
-get_sample_data.get_one_playlist()
+get_sample_data.get_all_playlists()
