1b92ae6ca1d58fb1ac975697485254faa8eaeb00
Huy <huy@hoang.dev>
2022-08-28 22:09:10 +0200

    refactoring

--- src/api_handler/playlists.py
+++ src/api_handler/playlists.py
@@ -58,7 +58,7 @@ class Playlists:
 
 		return pairs
 
-class LivePlaylist:
+class Playlist:
 	""" handles crud operations of a live playlist
 	the methods shouldnt be complicated
 	the bulk logic should be done by the api_handler \n
@@ -67,29 +67,36 @@ class LivePlaylist:
 	on the playlists on spotify.
 	"""
 	def __init__(self, playlist: AllPlaylists):
-		self.uri = playlist.uri
-		self.snapshot_id = playlist.snapshot_id
+		self.id = playlist.id
+		self._snapshot_id = playlist.snapshot_id
 		self.name = playlist.name
-		self._last_total_tracks = playlist.tracks.total
+		self._total_tracks = playlist.tracks.total
 
 	@property
 	def total_tracks(self):
-		data = sp.get_one_playlist(self.uri)
-		total_tracks = data.tracks.total
-		self._last_total_tracks = total_tracks
-		return total_tracks
+		# self._update_data()
+		return self._total_tracks
 
+	@property
+	def snapshot_id(self):
+		# self._update_data()
+		return self._snapshot_id
+
+	# def _update_data(self):
+	# 	data = sp.get_one_playlist(self.id)
+	# 	self._total_tracks = data.tracks.total
+	# 	self._snapshot_id = data.snapshot_id
 
 	def get_latest_tracks(self, num_tracks: int=0) -> list[PlaylistTracksItem]:
 		""" returns the latest n songs in playlist in added order.
 		That means the latest added song is at the end of the list\n
 		if num_songs == 0: return all songs in playlist """
 
-		if num_tracks == 0 or num_tracks > self._last_total_tracks:
+		if num_tracks == 0 or num_tracks > self._total_tracks:
 			num_tracks = self.total_tracks
 
 		tracks: list[PlaylistTracksItem]  = []
-		for playlist_tracks in sp.get_playlist_tracks_generator(self.uri):
+		for playlist_tracks in sp.get_playlist_tracks_generator(self.id):
 			if len(tracks) + len(playlist_tracks.items_) > num_tracks:
 				# TODO: remove hardcoded 100 below
 				rest = num_tracks % 100
@@ -101,7 +108,7 @@ class LivePlaylist:
 
 	def add_tracks_at_beginning(self, tracks: list[PlaylistTracksItem]):
 		track_ids = self.get_ids(tracks)
-		sp.add_tracks_at_beginning(self.uri, track_ids)
+		sp.add_tracks_at_beginning(self.id, track_ids)
 
 	def add_tracks_at_end(self, tracks: list[PlaylistTracksItem],
 															add_duplicates: bool = False):
@@ -114,15 +121,15 @@ class LivePlaylist:
 		# TODO: use add_duplicates to control if duplicates should be added
 
 		track_ids = self.get_ids(tracks)
-		sp.add_tracks_at_end(self.uri, track_ids, self.total_tracks)
+		sp.add_tracks_at_end(self.id, track_ids, self.total_tracks)
 
 	def remove_tracks(self, tracks: list[PlaylistTracksItem]):
 		track_ids = self.get_ids(tracks)
-		sp.remove_tracks(self.uri, track_ids)
+		sp.remove_tracks(self.id, track_ids)
 
 	def replace_tracks(self, tracks: list[PlaylistTracksItem]):
 		track_ids = self.get_ids(tracks)
-		sp.replace_playlist_tracks(self.uri, track_ids)
+		sp.replace_playlist_tracks(self.id, track_ids)
 
 	@staticmethod
 	def get_ids(tracks: list[PlaylistTracksItem]) -> list[str]:
