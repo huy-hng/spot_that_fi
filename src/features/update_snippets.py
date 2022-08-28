c5e0b4400c12bc9ac6f5babd22eced33642ace10
Huy <huy@hoang.dev>
2022-08-28 23:15:29 +0200

    remove old function and add pseudo logic

--- src/features/update_snippets.py
+++ src/features/update_snippets.py
@@ -3,42 +3,24 @@ from src import api_handler as api
 from src.controller.update_functions import update_all_playlist_tracks_in_db
 from src.db.playlists import get_track_ids
 from src.controller import playlist_change_detection as pcd
-from types.playlists import AllPlaylists
+from src.types.playlists import AllPlaylists
 
 # TODO move this to settings.json or .env
 SNIPPET_SIZE = 50
 
-def update_sp_snippet_playlists():
-	""" 
-	update all db playlists
-	get tracks from appropriate playlist in db
-	replace all tracks in playlist on spotify
-
-	alternate solution:
-	get x latest tracks directly from spotify
-	replace all tracks in playlist in spotify
-
-	this solution might be more friendly for rate limiting since
-	one doesnt need to update all playlists just to get
-	the snippet playlists
-	"""
-	update_all_playlist_tracks_in_db()
-
-	# get all main and snippet uris
-
-	uris = []
-	for uri in uris:
-		tracks = get_track_ids(uri)
-
 
 def sync_playlist_pair(main: AllPlaylists, snippet: AllPlaylists):
+	"""
+	TODO: sync playlists without knowing wether they have changed or not.
+	compare main and snippet playlists and see what has changed and
+	update both playlists approriately
+	"""
 	# prerequisite: know which playlists have changed
 	# if only main or snippet changed:
 	# 	add changes to other playlist
 	# if both playlists changed:
 	# 	get difference between both and update playlists
 	# update database
-	...
 
 	main_changed = pcd.has_playlist_changed(main)
 	snippet_changed = pcd.has_playlist_changed(snippet)
@@ -46,16 +28,30 @@ def sync_playlist_pair(main: AllPlaylists, snippet: AllPlaylists):
 	if main_changed and snippet_changed:
 		... # TODO
 	elif main_changed:
-		... # copy last x tracks from main to snippet
-		gen = sp.get_playlist_tracks_generator(main.id)
+		# copy last x tracks from main to snippet
+		main_playlist = api.playlists.Playlist(main)
+		tracks = main_playlist.get_latest_tracks(SNIPPET_SIZE)
+
+		snippet_playlist = api.playlists.Playlist(snippet)
+		snippet_playlist.replace_tracks(tracks)
+			
 	elif snippet_changed:
 		... # get diff between snippet and main
+		pcd.get_track_diff()
 	else:
 		... # nothing changed and can be skipped
 
 
 def sync_all_playlists():
-	""" bulk logic is in sync_playlists """
+	"""
+	update all db playlists
+	get tracks from appropriate playlist in db
+	replace all tracks in playlist on spotify
+
+	bulk logic is in sync_playlists
+	"""
+
+	update_all_playlist_tracks_in_db()
 	all_sp_playlists = sp.get_all_playlists()
 	playlists = api.Playlists(all_sp_playlists)
 
