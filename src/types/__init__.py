372874ea176c935e2d045c72ad1340a21cbd995a
Huy <huy@hoang.dev>
2022-09-03 18:49:03 +0200

    added todo

--- src/types/__init__.py
+++ src/types/__init__.py
@@ -1,4 +1,5 @@
 # from typing import Protocol
+# REFACTOR: use pydantic or something else that is more robust than this
 class DotDict(dict):
 	"""dot.notation access to dictionary attributes"""
 	__getattr__ = dict.get
