e0de6d57a8855434aa0d62c4dc50c7d510391fa9
Huy <huy@hoang.dev>
2022-09-03 23:27:20 +0200

    added test

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,7 +1,12 @@
 from src.api_handler import sp
+from src.tests import PlaylistIDs
 
 
 def test_get_liked_tracks():
 	for items in sp.get_liked_tracks_generator():
 		print(items)
 		break
+
+def test_get_one_playlist():
+	playlist = sp.get_one_playlist(PlaylistIDs.unchanged)
+	# playlist.
