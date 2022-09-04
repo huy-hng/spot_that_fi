bf8f1b8d128fa0684b82427f8380cd545145f060
Huy <huy@hoang.dev>
2022-09-05 00:41:22 +0200

    bunch of python testing for generic named tuples

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+# %%
+from copy import copy, deepcopy
+from typing import Generic, NamedTuple, TypeVar
+
+
 def named_tuple():
 	from typing import NamedTuple
 
@@ -57,6 +62,85 @@ def data_class():
 	print(data.data_1)
 	print(data.data_2.data2_1)
 
+
+T = TypeVar('T')
+
+
+class TestMetaclass(type):
+	def __new__(cls, name, bases, namespace: dict, **kwargs):
+		print(name)
+		print(bases)
+		print(namespace)
+		print(kwargs)
+		print()
+		return super().__new__(cls, name, bases, namespace, **kwargs)
+
+
+from typing import _NamedTuple, NamedTupleMeta
+class AllowGenericNamedTuple(type):
+	@staticmethod
+	def _namedtuple_mro_entries(bases):
+		assert bases[0] is NamedTuple
+		return (_NamedTuple,)
+
+	def __new__(cls, name, bases, namespace: dict, **kwargs):
+	
+		# GenericNamedTuple = NamedTuple
+		# GenericNamedTuple.__mro_entries__ = cls._namedtuple_mro_entries
+
+		# bases = (GenericNamedTuple, *bases)
+		print(bases)
+
+		# namespace['__orig_bases__'] = Generic[T]
+
+		# x = types.new_class(name, bases, **kwargs)
+		x = super().__new__(cls, name, bases, namespace, **kwargs)
+
+		return x
+		# return super().__new__(cls, name, bases, namespace, **kwargs)
+	def __init_subclass__(cls, a) -> None:
+		super().__init_subclass__()
+
+
+# def _namedtuple_mro_entries(bases):
+# 	assert bases[0] is GenericNamedTuple
+# 	return (_NamedTuple,)
+
+def _namedtuple_mro_entries(bases):
+	from typing import _GenericAlias, _NamedTuple
+	assert bases[0] is NamedTuple
+
+	if len(bases) > 1:
+		generic_only = all([isinstance(base, _GenericAlias) for base in bases[1:]])
+		if not generic_only:
+			raise TypeError("Multiple inheritance with NamedTuple is not supported")
+
+
+	return (_NamedTuple,)
+
+NamedTuple.__mro_entries__ = _namedtuple_mro_entries
+
+# class Test(GenericNamedTuple):
+# class Test(NamedTuple, dict):
+class Test(NamedTuple, Generic[T]):
+	a: T
+	b: str
+	c: str
+
+		
+
 if __name__ == '__main__':
-	data_class()
-	# named_tuple()
+
+	# test = GenericNamedTuple()
+	test = Test(True, 'asd', 'asd')
+	print(test)
+	# test.a
+	# test.b
+	# test.c
+
+
+	# pass
+	# test = NamedTuple('test', name=str, id=int)
+		# print(Test())
+	# print(type(tup))
+
