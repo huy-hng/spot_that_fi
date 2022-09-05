2f1758b5e1312833e8024d106e7d3146582b1649
Huy <huy@hoang.dev>
2022-09-05 02:32:16 +0200

    fix imports

--- src/db/features.py
+++ src/db/features.py
@@ -1,12 +1,9 @@
 from datetime import datetime, timedelta
 
-from sqlalchemy.orm import Session as sess
 from src.db.tables import Playlist
 from src.helpers.logger import log
 
-from . import SessionMaker
-
-# from .db_playlists
+from . import create_session
 
 
 # region create
@@ -14,8 +11,7 @@ def get_latest_tracks_added_to_playlists(days_old=14) -> list[str]:
 	""" returns a list with track_ids
 			that have been recently added to playlists """
 
-	with SessionMaker.begin() as session:
-		session: sess = session
+	with create_session() as session:
 		playlists = session.query(Playlist).all()
 
 		latest_tracks = []  # list with track_ids
