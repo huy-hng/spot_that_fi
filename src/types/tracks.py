67b52a27f1c8ae71699ec4073b4ed31c5b2ab24e
Huy <huy@hoang.dev>
2022-09-06 15:37:13 +0200

    add fields

--- src/types/tracks.py
+++ src/types/tracks.py
@@ -5,28 +5,28 @@ from src.types import init
 @dataclass(slots=True, frozen=True)
 class TrackDict:
 	""" previously called LikedTracksItemTrack """
-	album: dict
-	artists: list
-	available_markets: list
-	disc_number: int
-	duration_ms: int
+	album: dict = field(repr=False)
+	artists: list = field(repr=False)
+	available_markets: list = field(repr=False)
+	disc_number: int = field(repr=False)
+	duration_ms: int = field(repr=False)
 	# episode: bool # playlistTrack has this field
-	explicit: bool
-	external_ids: dict
-	external_urls: dict
-	href: str
+	explicit: bool = field(repr=False)
+	external_ids: dict = field(repr=False)
+	external_urls: dict = field(repr=False)
+	href: str = field(repr=False)
 	id: str
-	is_local: bool
+	is_local: bool = field(repr=False)
 	name: str
-	popularity: int
-	preview_url: str
+	popularity: int = field(repr=False)
+	preview_url: str = field(repr=False)
 	# track: bool # playlistTrack has this field
-	track_number: int
-	type: str
-	uri: str
+	track_number: int = field(repr=False)
+	type: str = field(repr=False)
+	uri: str = field(repr=False)
 
-	episode: bool | None = field(default=None) # playlistTrack has this field
-	track: bool | None = field(default=None)  # playlistTrack has this field
+	episode: bool | None = field(default=None, repr=False) # playlistTrack has this field
+	track: bool | None = field(default=None, repr=False)  # playlistTrack has this field
 
 		
 @dataclass(slots=True, frozen=True)
