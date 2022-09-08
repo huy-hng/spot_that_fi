8704545ef7c870c341ca9db84cc113e9d4423f6d
Huy <huy@hoang.dev>
2022-09-09 01:21:54 +0200

    added feature header

--- src/db/features.py
+++ src/db/features.py
@@ -1,18 +1,21 @@
 from datetime import datetime, timedelta
 
 from sqlalchemy.orm import Session
+from src import db
 from src.db import _, get_session
 from src.db.tables import Playlist
 from src.helpers.logger import log
 
 
 @get_session
-def get_latest_tracks_added_to_playlists(days_old=14, *, session: Session = _) -> list[str]:
-	""" returns a list with track_ids
-			that have been recently added to playlists """
-	playlists = session.query(Playlist).all()
+def get_latest_tracks_added_to_playlists(days_old=14, *, session: Session = _):
+	""" returns a list with track_ids that
+		have been recently added to playlists """
 
-	latest_tracks = []  # list with track_ids
+	playlists = session.query(Playlist).all()
+	# playlists = 
+
+	latest_tracks: list[str] = []  # list with track_ids
 	oldest_time = datetime.now() - timedelta(days=days_old)
 	for playlist in playlists:
 		tracks = playlist.playlist_track_association
@@ -21,3 +24,11 @@ def get_latest_tracks_added_to_playlists(days_old=14, *, session: Session = _) -
 				latest_tracks.append(track.track.id)
 
 	return latest_tracks
+
+
+@get_session
+def remove_unliked_tracks_not_in_playlists(*, session: Session = _):
+	""" remove tracks from db that aren't liked and
+		also don't belong to any playlists. """
+	
+	
