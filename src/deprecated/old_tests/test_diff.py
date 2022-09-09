d2e026626d53e5e56ca61c59174a547bf1ee9bef
Huy <huy@hoang.dev>
2022-09-09 23:56:52 +0200

    remove old files

--- src/deprecated/old_tests/test_diff.py
+++
@@ -1,7 +0,0 @@
-from src.controller.playlist_change_detection import update_db_playlist
-
-tech_house_id = '0oWDXsY9BhT9NKimKwNY9d'
-
-def diff():
-	update_db_playlist(tech_house_id)
-
--- src/deprecated/old_tests/test_get_tracks.py
+++
@@ -1,9 +0,0 @@
-from src import api
-from src.helpers.logger import log
-from src.helpers.helpers import write_data, read_data
-
-
-def test_get_tracks():
-	gen = api.get_playlist_tracks_generator('2OBconDUKoGs6BoDTVMVvk')
-	for batch in gen:
-		log.info(batch)
