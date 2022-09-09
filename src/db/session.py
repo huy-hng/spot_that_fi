7d77f8338c7a38e4732903fa8a27268e89de4851
Huy <huy@hoang.dev>
2022-09-09 19:11:09 +0200

    use parent session without input

--- src/db/session.py
+++ src/db/session.py
@@ -1,9 +1,13 @@
-
 from functools import wraps
 
 from sqlalchemy.orm import Session
-from src.db import SessionMaker
+from src.db.initializer import SessionMaker
+from src.helpers.logger import log
 
+
+_: Session = None  # type: ignore
+_session: Session = None  # type: ignore
+_parent_function: str = ''
 
 def create_session() -> Session:
 	return SessionMaker.begin()
@@ -12,13 +16,22 @@ def create_session() -> Session:
 def get_session(fn):
 	@wraps(fn)
 	def wrapper(*args, session=None, **kwargs):
+		global _session, _parent_function
 		if session is not None:
-			# print(f'using passed session for {fn.__name__}')
+			# log.debug(f'using passed session for {fn.__name__}')
 			return fn(*args, session=session, **kwargs)
 
+		elif _session is not None:
+			# log.debug(f'using {_parent_function} session for {fn.__name__}')
+			return fn(*args, session=_session, **kwargs)
+
 		with SessionMaker.begin() as session:
-			# print(f'creating new session for {fn.__name__}')
-			return fn(*args, session=session, **kwargs)
+			_session = session
+			_parent_function = fn.__name__
+			# log.debug(f'creating new session for {fn.__name__}')
+			result = fn(*args, session=session, **kwargs)
+			_session = None  # type: None
+			return result
 
 	return wrapper
 
